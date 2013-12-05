d7b0bdf659fad8ba18296b43ce4cdafa0e979c7c
Matt Story <mattstory@HUGENESS.local>
2013-12-04 19:15:14 -0500

    packaging mostly done for now
    Signed-off-by: Matt Story <mattstory@HUGENESS.local>

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@
    @license: BSD 3-Clause (see LICENSE.txt)
    @depends: flask, flask-sqlalchemy, numpy, requests, sqlite3, gunicorn
 '''
+from distutils.core import setup
 
 setup(
     name='stackpm',
     version='0.1b',
     author='Matthew Story',
+    packages=[ 'stackpm', ],
     author_email='matt.story@axial.net',
     url='https://github.com/axialmarket/stackpm',
     license='3-BSD',
     description='Stack Prioritization and Forecasting Utility',
     long_description=open('./README.rst').read(),
-    requires = [ 'flask', 'flask-sqlalchemy', 'numpy', 'requests', ]
+    requires=[ 'flask', 'flask.ext.sqlalchemy', 'numpy', 'requests', ]
 )
