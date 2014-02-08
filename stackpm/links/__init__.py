18ae02b0f29e6e6499471ad8d7fa75195120b966
Matthew Story <matt.story@axialmarket.com>
2014-02-08 10:36:38 -0500

    memoize connectors, make config more explicit
    Signed-off-by: Matthew Story <matt.story@axialmarket.com>

--- stackpm/links/__init__.py
+++ stackpm/links/__init__.py
@@ -10,21 +10,28 @@ import noop
 ### GLOBALS
 # each of these ends up being a module level variable
 LINK_TYPES = ( 'project_manager', 'scm', 'calendar', )
-CONNECTORS = stackpm_app.config.get('links', {}).get('links', {})
+LINK_CONFIG = stackpm_app.config.get('links', {})
+CONNECTORS = LINK_CONFIG.get('connectors', {})
 
 # DEPENDS CONNECTORS, noop
 import connectors
 
 # DEPENDS connectors
 def setup():
-    '''Setup package-level links for all link-types defined in LINKS section'''
-    link_conf = stackpm_app.config.get('links', {})
-    _scope = globals()
+    '''Setup package-level links for all link-types defined in links
+       section. Note that if multiple link types specify the same connector,
+       a single connector object will be shared across all link types.'''
+    conns = {}
+    scope = globals()
     for link_type in LINK_TYPES:
-        link = link_conf.get(link_type) or 'noop'
+        link = LINK_CONFIG.get(link_type) or 'noop'
         config = stackpm_app.config.get(link, {})
-        connector = getattr(connectors, link).Connector(config)
-        _scope[link_type] = connector
+        try:
+            connector = conns[link]
+        except KeyError:
+            conns[link] = getattr(connectors, link).Connector(config)
+            connector = conns[link]
+        scope[link_type] = connector
 
 setup()
 del setup
